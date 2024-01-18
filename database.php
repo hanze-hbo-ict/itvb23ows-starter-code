b88890c9b6a89e55b73e1718df402f9e7ee9ecc6
maaike <maaikeverloop@gmail.com>
2024-01-18 22:45:45 +0100

    code smell fix - database.php

--- database.php
+++ database.php
@@ -1,17 +1,16 @@
 <?php
 
-function get_state() {
+function getState(): string
+{
     return serialize([$_SESSION['hand'], $_SESSION['board'], $_SESSION['player']]);
 }
 
-function set_state($state) {
+function setState($state): void
+{
     list($a, $b, $c) = unserialize($state);
     $_SESSION['hand'] = $a;
     $_SESSION['board'] = $b;
     $_SESSION['player'] = $c;
 }
 
-//return new mysqli('172.17.0.2', 'root', '', 'hive');
 return new mysqli('mysql', 'root', '', 'hive');
-
-?>
