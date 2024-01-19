461412d5dec5d7f73b7f04a87cffd211f72313de
maaike <maaikeverloop@gmail.com>
2024-01-19 17:50:13 +0100

    code smell fix - util.php - rewrote isNeighbour, added {}

--- util.php
+++ util.php
@@ -6,16 +6,19 @@ function isNeighbour($a, $b): bool
 {
     $a = explode(',', $a);
     $b = explode(',', $b);
-    if ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1) return true;
-    if ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) return true;
-    if ($a[0] + $a[1] == $b[0] + $b[1]) return true;
-    return false;
+    return (
+        ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1) or
+        ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) or
+        ($a[0] + $a[1] == $b[0] + $b[1])
+    );
 }
 
 function hasNeighbour($a, $board): bool
 {
     foreach (array_keys($board) as $b) {
-        if (isNeighbour($a, $b)) return true;
+        if (isNeighbour($a, $b)) {
+            return true;
+        }
     }
     return false;
 }
@@ -23,9 +26,13 @@ function hasNeighbour($a, $board): bool
 function neighboursAreSameColor($player, $a, $board): bool
 {
     foreach ($board as $b => $st) {
-        if (!$st) continue;
+        if (!$st) {
+            continue;
+        }
         $c = $st[count($st) - 1][0];
-        if ($c != $player && isNeighbour($a, $b)) return false;
+        if ($c != $player && isNeighbour($a, $b)) {
+            return false;
+        }
     }
     return true;
 }
@@ -37,15 +44,23 @@ function len($tile): int
 
 function slide($board, $from, $to): bool
 {
-    if (!hasNeighbour($to, $board)) return false;
-    if (!isNeighbour($from, $to)) return false;
+    if (!hasNeighbour($to, $board)) {
+        return false;
+    }
+    if (!isNeighbour($from, $to)) {
+        return false;
+    }
     $b = explode(',', $to);
     $common = [];
     foreach ($GLOBALS['OFFSETS'] as $pq) {
         $p = $b[0] + $pq[0];
         $q = $b[1] + $pq[1];
-        if (isNeighbour($from, $p.",".$q)) $common[] = $p.",".$q;
+        if (isNeighbour($from, $p.",".$q)) {
+            $common[] = $p.",".$q;
+        }
     }
-    if (!$board[$common[0]] && !$board[$common[1]] && !$board[$from] && !$board[$to]) return false;
+    if (!$board[$common[0]] && !$board[$common[1]] && !$board[$from] && !$board[$to]) {
+        return false;
+    }
     return min(len($board[$common[0]]), len($board[$common[1]])) <= max(len($board[$from]), len($board[$to]));
 }
