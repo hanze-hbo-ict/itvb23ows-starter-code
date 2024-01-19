94dd77163c7235861cac002132609c0eb1d038b1
maaike <maaikeverloop@gmail.com>
2024-01-19 17:20:42 +0100

    code smell fix - index.php - remove echo parentheses

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
         </form>
         <strong>
             <?php if (isset($_SESSION['error'])) {
-                echo($_SESSION['error']);
+                echo $_SESSION['error'];
                 unset($_SESSION['error']);
             } ?>
         </strong>
