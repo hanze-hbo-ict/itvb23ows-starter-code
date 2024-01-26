4ee4e93d020916155ebaab5cf169568f116a650f
maaike <maaikeverloop@gmail.com>
2024-01-26 20:38:46 +0100

    added getPossiblePlayPositions test

--- app/tests/BoardTest.php
+++ app/tests/BoardTest.php
@@ -21,15 +21,27 @@ class BoardTest extends PHPUnit\Framework\TestCase
         self::assertEquals(['0,0'], $possiblePlaypositions);
     }
 
-    public function testGivenOneTileThenGetPossiblePlayPositionReturnSixSides() {
+    public function testGivenOneTileThenGetPossiblePlayPositionsReturnSixSides() {
         $boardTiles = ['0,0' => [[0, "Q"]]];
         $board = new Board($boardTiles);
-        $playerNumber = 0;
+        $playerNumber = 1;
         $hand = ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3];
 
         $possiblePlaypositions = $board->getPossiblePlayPositions($playerNumber, $hand);
 
         $expectedResult = [0 => '0,1', 1 => '0,-1', 2 => '1,0', 3 => '-1,0', 4 => '-1,1', 5 => '1,-1'];
+        self::assertEquals($expectedResult, $possiblePlaypositions);
+    }
+
+    public function testGivenTwoTilesThenGetPossiblePlayPositionsReturnSides() {
+        $boardTiles = ['0,0' => [[0, "Q"]], '0,1' => [[1, "Q"]]];
+        $board = new Board($boardTiles);
+        $playerNumber = 0;
+        $hand = ["B" => 2, "S" => 2, "A" => 3, "G" => 3];
+
+        $possiblePlaypositions = $board->getPossiblePlayPositions($playerNumber, $hand);
+
+        $expectedResult = [0 => '0,-1', 1 => '-1,0', 2 => '1,-1'];
         self::assertEquals($expectedResult, $possiblePlaypositions);
     }
 
