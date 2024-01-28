e183475d34a4551c05eae9583a7e7dcb7a14ea92
joyce0398 <joyce0398@msn.com>
2024-01-28 18:07:11 +0100

    implemented sonarqube code issues

--- hive_game/pass.php
+++ hive_game/pass.php
@@ -2,13 +2,12 @@
 
 session_start();
 
-$db = include 'database.php';
-$stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "pass", null, null, ?, ?)');
+$db = include_once 'database.php';
+$stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state)
+values (?, "pass", null, null, ?, ?)');
 $stmt->bind_param('iis', $_SESSION['game_id'], $_SESSION['last_move'], get_state());
 $stmt->execute();
 $_SESSION['last_move'] = $db->insert_id;
 $_SESSION['player'] = 1 - $_SESSION['player'];
 
 header('Location: index.php');
-
-?>
