96dff4c3303e8aa47714536afe36c77369b72988
joyce0398 <joyce0398@msn.com>
2024-01-31 21:34:31 +0100

    Refactored restart

--- hive_game/src/restart.php
+++ hive_game/src/restart.php
@@ -1,15 +1,17 @@
 <?php
 
 session_start();
-use HiveGame\Database;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Joyce0398\HiveGame\Database;
 
 $_SESSION['board'] = [];
 $_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3],
  1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
 $_SESSION['player'] = 0;
 
-$db = new Database();
-$db->prepare('INSERT INTO games VALUES ()')->execute();
-$_SESSION['game_id'] = $db->insert_id;
+$insertId = Database::restart();
+$_SESSION['game_id'] = $insertId;
 
 header('Location: index.php');
